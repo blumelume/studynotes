da47807d409e401d523b05dcabdbdccc578756bb
Maximilian Rudolph <rudolph-max@gmx.net>
2023-07-13 14:53:53 +0200

    using path alias & https dev-server

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
+import path from 'path'
+import mkcert from 'vite-plugin-mkcert'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    base: '',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, '/src'),
+        },
+    },
+    server: {
+        host: true,
+        https: true,
+    },
+    plugins: [
+        mkcert(),
+        vue()
+    ],
 })
